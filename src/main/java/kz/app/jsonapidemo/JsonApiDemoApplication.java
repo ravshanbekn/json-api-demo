957a2ba67ffeac58fbea834dfa0c2957c98dd52d
Ravshanbek <ravshanbeknabiyev.dev@gmail.com>
2026-05-10 23:58:47 +0500

    init

---
+++ src/main/java/kz/app/jsonapidemo/JsonApiDemoApplication.java
@@ -0,0 +1,13 @@
+package kz.app.jsonapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonApiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonApiDemoApplication.class, args);
+    }
+
+}
